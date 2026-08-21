a03cf40c7b9c779e865be4e1a59b49541cfabe6c
chene <121494822+chenetulipe@users.noreply.github.com>
2026-08-21 16:03:16 +0200

    chore: remove temporary script files

--- check.py
+++
@@ -1,5 +0,0 @@
-import sys
-with open('p2is_tool/server/src/encoders/bin_encoder.py', 'r', encoding='utf-8') as f:
-    text = f.read()
-idx = text.find('t_fr = \\'\\'.join(tokens) + "..."')
-print(text[idx-200:idx+200].encode('ascii', 'backslashreplace').decode('ascii'))
